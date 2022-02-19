94c305ef909b2c4ad9158e873f22c5622952b8b4
Ashutosh Singh <ashutosh.singh19071999@gmail.com>
2022-02-19 23:11:32 +0530

    Splash Screen

---
+++ app/src/main/java/com/kotlinapp/swiggyclone/splashActivity/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.kotlinapp.swiggyclone.splashActivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.kotlinapp.swiggyclone.R
+import com.kotlinapp.swiggyclone.databinding.ActivitySplashBinding
+import com.kotlinapp.swiggyclone.homeScreen.HomeActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    //Intializing the view binding
+    lateinit var activitySplashBinding : ActivitySplashBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+
+        setContentView(activitySplashBinding.root)
+
+
+        var handler = Handler().postDelayed(Runnable {
+
+            var intentHome = Intent(this,HomeActivity::class.java)
+            startActivity(intentHome)
+
+
+        },1000)
+
+
+    }
+}
