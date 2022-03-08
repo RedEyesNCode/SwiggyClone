22c397fc0d3229ef0fa391aca104ff6119c334c2
Ashutosh Singh <ashutosh.singh19071999@gmail.com>
2022-03-08 23:44:50 +0530

    Spring intiazr

---
+++ SwiggySpringBoot/swiggyClone/src/test/java/com/swiggy/swiggyClone/SwiggyCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiggy.swiggyClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiggyCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
