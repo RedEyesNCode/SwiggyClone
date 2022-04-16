6f9e063cf88638ef959f5e05f1700a58e3edd6b3
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2022-04-16 11:31:12 +0530

    Shared Preferences Update

--- app/src/main/java/com/kotlinapp/swiggyclone/sharedPreferences/Constant.kt
+++ app/src/main/java/com/kotlinapp/swiggyclone/sharedPreferences/Constant.kt
@@ -4,9 +4,11 @@ class Constant {
 
 
 
+    //THE CONSTANT CLASS IS NOT THE KEY VALUE CLASS IT JUST STORES THE KEY.
+    //THE VALUE TO THE KEY IS GIVEN BY DEVELOPER.
     var PREFERENCES_NAME : String= "SWIGGY_CLONE"
-    var ACCESS_TOKEN : String = ""
-    var USER_ID : String = ""
+    var ACCESS_TOKEN : String = "ACCESS_TOKEN"
+    var USER_ID : String = "USER_ID"
 
 
 
